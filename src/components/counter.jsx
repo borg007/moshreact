71d38f304ad8151e1e1e227b88ffb099d524543d
Marlon Benjamin <marlonben@gmail.com>
2020-06-07 21:38:51 -0400

    Conditional Rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -15,7 +15,8 @@ class Counter extends Component {
     render () {  
         return (
             <div>
-            {this.renderTags()}
+                {this.state.tags.length === 0 && "Please create a new tag!"}
+                {this.renderTags()}
             </div>
         );
     }
