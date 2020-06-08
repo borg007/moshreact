c0d02922ff404d7b2af6e72215f5d38de19a9682
Marlon Benjamin <marlonben@gmail.com>
2020-06-07 22:01:21 -0400

    Handling Events

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,15 +5,24 @@ class Counter extends Component {
         count: 0
     };
 
+    handleIncrement() {
+        console.log('Increment Clicked');
+    }
+
     render () {  
         return (
             <div>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
+                <button 
+                    onClick={this.handleIncrement} 
+                    className="btn btn-secondary btn-sm"
+                >
+                    Increment
+                </button>
             </div>
         );
     }
-    
+
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
         classes += this.state.count === 0 ? "warning" : "primary";
