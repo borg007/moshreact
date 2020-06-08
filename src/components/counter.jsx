ac6f67b6631ff217ca1cc2d6ff9ee13804597974
Marlon Benjamin <marlonben@gmail.com>
2020-06-07 20:53:23 -0400

    Setting Attributes - Styles

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
     state = {
-        count: 0,
+        count: 1
     };
 
     styles = {
@@ -11,9 +11,12 @@ class Counter extends Component {
     };
 
     render () {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        
         return (
             <div>
-                <span style={{fontSize: 30}} className="badge badge-primary m-2">{this.formatCount()}</span>
+                <span className={classes}>{this.formatCount()}</span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
             </div>
         );
