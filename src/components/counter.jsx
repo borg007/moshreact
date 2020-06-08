63f82403097f196840a105941cdc959bdf1d2f47
Marlon Benjamin <marlonben@gmail.com>
2020-06-07 20:26:52 -0400

    Setting Attributes - Styles

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,10 +4,16 @@ class Counter extends Component {
     state = {
         count: 0,
     };
+
+    styles = {
+        fontSize: 20,
+        fontWeight: "bold"
+    };
+
     render () {
         return (
             <div>
-                <span className="badge badge-primary m-2">{this.formatCount()}</span>
+                <span styles={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
             </div>
         );
