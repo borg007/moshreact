e49bdf10e51ed3d6d22b2065d3910c1678a2e063
Marlon Benjamin <marlonben@gmail.com>
2020-06-09 18:08:11 -0400

    Removing the Local State

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,19 +1,6 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-    state = {
-        value: this.props.counter.value
-    };
-
-   /* constructor () {
-        super();
-        this.handleIncrement = this.handleIncrement.bind(this);
-    }*/
-
-    handleIncrement = () => {
-        this.setState({ value: this.state.value + 1 })
-    };
-    
     render () {  
         return (
             <div>
