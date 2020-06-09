af9478386de920f6712376ed40ef750968f5967d
Marlon Benjamin <marlonben@gmail.com>
2020-06-09 19:14:23 -0400

    Multiple Components in Sync

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counters from './components/counters';
 
-ReactDOM.render(<Counters />, document.getElementById("root"));
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
